096beb39bcce61176b5fcfbc2b875ec29486e3d5
masonjadie <masonjadie@users.noreply.github.com>
2026-04-15 22:34:53 -0500

    Perf: Optimize LCP by inlining critical Hero CSS and flattening routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
+  { 
+    path: '', 
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
+  },
+  { 
+    path: 'home', 
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
+  },
+  { 
+    path: 'auth', 
+    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) 
+  },
   {
     path: '',
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
